13e0b377ddce9a6426d97ce7f11e76976d0a82a0
Remigijus Kiminas <remdex@gmail.com>
2019-03-04 09:02:31 -0500

    Use instance

--- design/lhctelegramtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
+++ design/lhctelegramtheme/tpl/pagelayouts/parts/modules_menu/extension_module_multiinclude.tpl.php
@@ -1,3 +1,3 @@
-<?php if ($currentUser->hasAccessTo('lhtelegram','use_admin')) : ?>
+<?php if (erLhcoreClassUser::instance()->hasAccessTo('lhtelegram','use_admin')) : ?>
 <li class="nav-item"><a class="nav-link" href="<?php echo erLhcoreClassDesign::baseurl('telegram/index')?>"><i class="material-icons">textsms</i><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('pagelayout/pagelayout','Telegram settings');?></a></li>
 <?php endif; ?>
